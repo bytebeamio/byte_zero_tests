912eaad8d894b4c8fb07e5cf05e882d8794e094e
amokfa <iaansagar@gmail.com>
2023-12-11 15:48:10 +0530

    initial commit

---
+++ android/app/src/main/kotlin/io/bytebeam/byte_zero_tests/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.bytebeam.byte_zero_tests
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ByteZeroTestApp());
+}
+
+class ByteZeroTestApp extends StatelessWidget {
+  const ByteZeroTestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child: Text("To be implemented")
+        ),
+      ),
+    );
+  }
+}
